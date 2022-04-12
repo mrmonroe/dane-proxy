615f97dafbc520c0d022a4f73c81cd44defae048
Matthew Monroe <matthewryan.monroe@gmail.com>
2022-04-12 18:39:21 -0400

    Splitting out into more classes trying to make it easier to manage  On branch feature/base  Changes to be committed: 	deleted:    lib/ProxyServer.js 	deleted:    lib/index.js 	deleted:    lib/logger.js 	modified:   src/ProxyServer.ts 	new file:   src/ProxySocket.ts 	modified:   src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import ProxyServer from './ProxyServer';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 try {
   const serve: ProxyServer = new ProxyServer({ port: 8080 });
+  if (serve) {
+    console.log('Dane Proxy is listening!', 8080);
+  }
 } catch (e) {
   throw new Error('Cannot Start check dependencies');
 }
